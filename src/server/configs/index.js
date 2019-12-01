b184a205b348a8d1df80a9010691dacfecec7659
Fatih Turgut <fatih.turgut@akinon.com>
2019-12-02 01:06:59 +0300

    config

--- src/server/configs/index.js
+++ src/server/configs/index.js
@@ -1,5 +1,5 @@
-const user = process.env.USER || 'fturgut';
-const password = process.env.PASSWORD || 'ft123456';
+const user = process.env.DB_USER || 'fturgut';
+const password = process.env.DB_PASSWORD || 'ft123456';
 
 module.exports.DATABASE = {
   uri: `mongodb://${user}:${password}@ds027521.mlab.com:27521/react-node-todo-app`,
